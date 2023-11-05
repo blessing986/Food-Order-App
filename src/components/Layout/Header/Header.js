6b7a93342a0166a91d988ebba151deaa57b75666
Blessing U <blessing986@users.noreply.github.com>
2023-11-05 15:28:04 +0000

    chore: managing cart and modal state

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={styles.header}>
         <h1>Blessing's_Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
 
       <div className={styles["main-image"]}>
--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -3,14 +3,12 @@ import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={styles.button}>
-
+    <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={styles.badge}>3</span>
-      
     </button>
   );
 };
