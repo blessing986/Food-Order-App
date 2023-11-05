e0f37f01c3c692c7c1927cafea6570e359ab60c0
Blessing U <blessing986@users.noreply.github.com>
2023-11-05 14:03:20 +0000

    chore: added cart button component

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 
 import mealsImage from "../../../assets/meals.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Blessing's_Meals</h1>
+        <HeaderCartButton />
       </header>
 
       <div className={styles["main-image"]}>
