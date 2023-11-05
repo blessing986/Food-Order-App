daa210b4c2c9f57e6e1b6f4b89334da788fc8094
Blessing U <blessing986@users.noreply.github.com>
2023-11-05 15:13:16 +0000

    chore: added a modal via react portal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
-
       <main>
         <Meals />
       </main>
